7f0736c95139c2a01f13dbc0e222c29c07f47698
bgrover281 <bgrover281@gmail.com>
2026-07-23 20:46:01 -0500

    Pipes working, Artwork added
    created corresponding top pipes. Added pipe and bird artwork. Need to add score function.

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 
-const birdImg = new Image();
-birdImg.src = 'files/flappy bird.png';
+const planeImg= new Image();
+planeImg.src = 'files/plane.png.png';
 
+const pipeImg = new Image();
+pipeImg.src = 'files/pipe.png';
 
+const topPipeImg = new Image();
+topPipeImg.src = 'files/topPipe.png';
 
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
-birdImg.onload = () => {
+planeImg.onload = () => {
 ctx.fillStyle = 'yellow';
 ctx.fillRect(50, 200, 30, 30);
-ctx.drawImage(birdImg, 50, 200, 34, 24);
+ctx.drawImage(planeImg, 50, 200, 100, 100);
 }
 
 window.addEventListener('keydown', (e) => {
@@ -23,35 +27,36 @@ let birdX = 50;
 let birdY = 200;
 let velocity = 0;
 
-let pipex = canvas.width; // pipe starts at the right edge of the canvas
-let pipey = 250; // pipe height
-let pipeWidth = 50; // pipe width
 let pipHeight = 200; // pipe height
+let pipex = canvas.width; // pipe starts at the right edge of the canvas
+let pipey = canvas.height - pipHeight; // pipe height
+let pipeWidth = 50; // pipe width
 const gravity = 0.4;    
 
 function update() {
     velocity += gravity; // Movement
     birdY += velocity; // apply movement
-    pipex -= 2; // move pipe to the left
+    pipex -= 3; // move pipe to the left
 
 
  ctx.clearRect(0, 0, canvas.width, canvas.height); // clear the canvas
 
     ctx.fillStyle = 'green'; // set pipe color
-    ctx.fillRect(pipex, pipey, pipeWidth, pipHeight); // draw pipe
+    ctx.drawImage(pipeImg, pipex, pipey, pipeWidth, pipHeight); // draw pipe
 
     if (pipex < -50) { // reset pipe position when it goes off screen
         pipex = canvas.width;
-        pipey = Math.random() * (canvas.height - 200); // randomize pipe height
+        pipHeight = Math.random() * 500 + 35;  // randomize pipe height
+        pipey = canvas.height - pipHeight; // reset pipe y position
     }
-   
-    ctx.drawImage(birdImg, birdX, birdY, 34, 24); // draws bird 34 width and 24 height
+   ctx.drawImage(topPipeImg, pipex, 0, pipeWidth, pipey - 150); // draw top pipe
+    ctx.drawImage(planeImg, birdX, birdY, 55, 40); // draws plane 34 width and 24 height
     
     requestAnimationFrame(update); // loop 
 
 
 }
 
-birdImg.onload = () => {
+planeImg.onload = () => {
     update(); // Start game loop
 }
